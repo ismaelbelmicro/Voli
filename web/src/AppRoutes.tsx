d7c314bf6599ed69e1b5b78389091f14c0426b68
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-04 11:54:57 -0300

    Rota privada

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -5,22 +5,25 @@ import PaginaInicial from "./pages/PaginaInicial";
 import PaginaBaseFormulario from "./pages/PaginaBaseFormulario";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
+import RotaPrivada from "./utils/RotaPrivada";
 
 function AppRoutes() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<PaginaBase />}>
-            <Route index element={<PaginaInicial />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<PaginaInicial />} />
+          <Route element={<RotaPrivada />}>
             <Route path="/dashboard" element={<Dashboard />} />
-            </Route>
-            <Route path="/" element={<PaginaBaseFormulario />}>
-                <Route path="/login" element={<Login />} />
-                <Route path="/cadastro" element={<Cadastro />} />
-            </Route>
-        </Routes>
-        </BrowserRouter>
-    )
+          </Route>
+        </Route>
+        <Route path="/" element={<PaginaBaseFormulario />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
