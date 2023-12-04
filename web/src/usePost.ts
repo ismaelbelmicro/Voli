e84c4afb5211083c09b8c89ad0e9b58b041a1fb8
ismaelbelmicro <ismael.patrick@belmicro.com.br>
2023-12-04 11:48:10 -0300

    Armazenando token

--- web/src/usePost.ts
+++ web/src/usePost.ts
@@ -3,19 +3,22 @@ import { useState } from "react";
 export default function usePost() {
   const [erro, setErro] = useState("");
   const [sucesso, setSucesso] = useState(false);
+  const [resposta, setReposta] = useState("");
 
   async function cadastrarDados<T>({ url, dados }: { url: string; dados: T }) {
     try {
-      await fetch(`http://localhost:8080/${url}`, {
+      const resposta = await fetch(`http://localhost:8080/${url}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(dados),
       });
       setSucesso(true);
+      const respostaConvertida = await resposta.json();
+      setReposta(respostaConvertida.token);
     } catch (erro) {
       setErro("Não foi possível enviar os dados");
     }
   }
 
-  return { cadastrarDados, sucesso, erro };
+  return { cadastrarDados, sucesso, erro, resposta };
 }
